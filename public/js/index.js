d0963fee17bff681498fffbcabfe90133da25309
Damon Oehlman <damon.oehlman@gmail.com>
2014-02-24 12:45:39 +1100

    Use fabric from cdnjs instead of npm modules

--- public/js/index.js
+++ public/js/index.js
@@ -4,7 +4,8 @@ var crel = require('crel');
 var uuid = require('uuid');
 var opts = {
   config: {
-    iceServers: []
+    iceServers: [
+    ]
   }
 };
 
@@ -16,99 +17,97 @@ var cdndeps = [
   '//cdnjs.cloudflare.com/ajax/libs/fabric.js/1.4.0/fabric.min.js'
 ];
 
-loader(cdndeps, function() {
-  function joined(err, m) {
-    function addObject(obj, label) {
-      // add the object to the canvas
-      canvas.add(obj);
+function joined(err, m) {
+  function addObject(obj, label) {
+    // add the object to the canvas
+    canvas.add(obj);
 
-      // tag the object
-      obj._label = label || uuid.v4();
+    // tag the object
+    obj._label = label || uuid.v4();
 
-      // add the object into mesh data
-      m.data.set(obj._label, obj.toJSON());
+    // add the object into mesh data
+    m.data.set(obj._label, obj.toJSON());
+  }
+
+  function updateState(evt) {
+    // TODO: debounce
+    if (evt.target && evt.target._label) {
+      m.data.set(evt.target._label, evt.target.toJSON());
+    }
+  }
+
+  function remoteUpdate(data, clock, srcId) {
+    var key;
+    var target;
+
+    if (srcId === m.id) {
+      return;
     }
 
-    function updateState(evt) {
-      // TODO: debounce
-      if (evt.target && evt.target._label) {
-        m.data.set(evt.target._label, evt.target.toJSON());
-      }
-    }
+    key = data[0];
+    target = canvas._objects.filter(function(obj) {
+      return obj._label === key;
+    })[0];
 
-    function remoteUpdate(data, clock, srcId) {
-      var key;
-      var target;
-
-      if (srcId === m.id) {
-        return;
-      }
-
-      key = data[0];
-      target = canvas._objects.filter(function(obj) {
-        return obj._label === key;
-      })[0];
-
-      // if we don't have the target, then add the target
-      if (! target) {
-        // use fabric deserialization
-        fabric.util.enlivenObjects([data[1]], function(objects) {
-          objects[0]._label = key;
-          canvas.add(objects[0]);
-        });
-      }
-      else {
-        target.set(data[1]);
-        canvas.renderAll();
-      }
-    }
-
-    canvas.on('object:modified', updateState);
-
-    // if dynamic updates are enabled then communicate changes
-    // as they are happening in the UI
-    if (dynamicUpdates) {
-      canvas.on({
-        'object:moving': updateState,
-        'object:scaling': updateState,
-        'object:rotating': updateState
+    // if we don't have the target, then add the target
+    if (! target) {
+      // use fabric deserialization
+      fabric.util.enlivenObjects([data[1]], function(objects) {
+        objects[0]._label = key;
+        canvas.add(objects[0]);
       });
     }
+    else {
+      target.set(data[1]);
+      canvas.renderAll();
+    }
+  }
 
-    m.data.on('update', remoteUpdate);
+  canvas.on('object:modified', updateState);
 
-    m.on('sync', function() {
-      console.log('synced');
-
-      if (m.data.get('startrect')) {
-        return;
-      }
-
-      addObject(new fabric.Text('hello', {
-        left: 210,
-        top: 100
-      }), 'testlabel');
-
-      addObject(new fabric.Rect({
-        left: 100,
-        top: 100,
-        fill: 'blue',
-        height: 100,
-        width: 100
-      }), 'startrect');
+  // if dynamic updates are enabled then communicate changes
+  // as they are happening in the UI
+  if (dynamicUpdates) {
+    canvas.on({
+      'object:moving': updateState,
+      'object:scaling': updateState,
+      'object:rotating': updateState
     });
   }
 
-  function initCanvas() {
-    document.body.appendChild(crel('canvas', {
-      id: 'c',
-      width: window.innerWidth,
-      height: window.innerHeight
-    }));
+  m.data.on('update', remoteUpdate);
 
-    canvas = new fabric.Canvas('c');
-    mesh.join('rtc-mesh-drawtest', opts, joined);
-  }
+  m.on('sync', function() {
+    console.log('synced');
 
-  window.onload = initCanvas;
-});
+    if (m.data.get('startrect')) {
+      return;
+    }
+
+    addObject(new fabric.Text('hello', {
+      left: 210,
+      top: 100
+    }), 'testlabel');
+
+    addObject(new fabric.Rect({
+      left: 100,
+      top: 100,
+      fill: 'blue',
+      height: 100,
+      width: 100
+    }), 'startrect');
+  });
+}
+
+function initCanvas() {
+  document.body.appendChild(crel('canvas', {
+    id: 'c',
+    width: window.innerWidth,
+    height: window.innerHeight
+  }));
+
+  canvas = new fabric.Canvas('c');
+  mesh.join('rtc-mesh-drawtest', opts, joined);
+}
+
+loader(cdndeps, initCanvas);
